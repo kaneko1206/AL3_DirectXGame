7cf2a3db2e79a820fa26583f8e176e1611ab7f31
kaneko <k022g1074@g.neec.ac.jp>
2023-04-17 15:02:05 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2A_08_カネコ_ショウ");
+	win->CreateGameWindow(L"GC2A_08_カネコ_ショウ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
